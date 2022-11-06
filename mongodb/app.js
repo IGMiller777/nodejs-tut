96903a44bbd1a5848597b6a06ccb66412655d90f
Ivan Gamanovich <igamanovich@elinext.com>
2022-11-06 22:05:48 +0300

    feat: creare mongodb

---
+++ mongodb/app.js
@@ -0,0 +1,21 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://localhost:27017/";
+const mongoClient = new MongoClient(url);
+
+// Подключаемся к серверу
+mongoClient.connect(function(err, client){
+
+    // обращаемся к базе данных admin
+    const db = client.db("admin");
+
+    db.command({ping: 1}, function(err, result){
+        if(!err){
+            console.log("Подключение с сервером успешно установлено");
+            console.log(result);
+        }
+        // Закрываем подключение
+        client.close();
+        console.log("Подключение закрыто");
+    });
+});
